90b9ee5449728cc00f3e74aaaf2c02d019206381
TiagoLobo-source <65892221+TiagoLobo-source@users.noreply.github.com>
2023-07-29 16:00:09 +0100

    model included

--- models/User.model.js
+++ models/User.model.js
@@ -1,31 +1,52 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      trim: true,
-      required: false,
-      unique: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true
-    }
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
-  }
-);
+  passwordHash: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  repeatPassword: {
+    type: String,
+    required: [true, "Password does not match"],
+  },
+
+  isJobseeker: {
+    type: Boolean,
+    default: true,
+  },
+
+  firstName: {
+    type: String,
+    required: [true, "First Name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last Name is required"],
+  },
+  aboutMe: {
+    type: String,
+  },
+
+  dateOfBrith: {
+    type: Date,
+  },
+  professionalExperience: {
+    type: String,
+  },
+
+  companyLocation: {
+    type: String,
+    required: [true, "Location is required"],
+  },
+});
 
 const User = model("User", userSchema);
 
